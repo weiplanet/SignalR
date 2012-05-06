421e04363aca57697b235b1f3062c384c2d66acd
David Fowler <davidfowl@gmail.com>
2012-05-05 20:54:12 -0700

    Fix race condition in WebApiResponse. - Keep a single task around that represents the operation where the stream is captured.

--- SignalR.Hosting.WebApi/WebApiResponse.cs
+++ SignalR.Hosting.WebApi/WebApiResponse.cs
@@ -14,7 +14,8 @@ namespace SignalR.Hosting.WebApi
         private readonly HttpResponseMessage _responseMessage;
         private readonly Action _sendResponse;
 
-        private int streamingInitialized;
+        private Task _beginWriteTask;
+
         private bool _writeFailed;
         private bool _ended;
         private Stream _stream;
@@ -39,37 +40,45 @@ namespace SignalR.Hosting.WebApi
         public Task EndAsync(string data)
         {
             _responseMessage.Content = new StringContent(data);
-            
+
             if (!String.IsNullOrEmpty(ContentType))
             {
                 _responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue(ContentType);
             }
-            
+
             return TaskAsyncHelper.Empty;
         }
 
         public Task WriteAsync(string data)
         {
-            if (Interlocked.Exchange(ref streamingInitialized, 1) == 0)
+            // If the stream is ready then just write
+            if (_stream != null)
             {
-                var tcs = new TaskCompletionSource<object>();
-                _responseMessage.Content = new PushStreamContent((stream, contentHeaders, context) =>
-                {
-                    _stream = stream;
-
-                    tcs.TrySetResult(null);
-                },
-                new MediaTypeHeaderValue(ContentType));
-
-                // Return the response back to the client
-                _sendResponse();
-
-                // Write when the stream is ready
-                return tcs.Task.Then(() => WriteTaskAsync(data)).Catch();
+                return WriteTaskAsync(data).Catch();
             }
 
-            // The stream has already been initialized so just write
-            return WriteTaskAsync(data).Catch();
+            // Make sure we don't miss a write here in case write is called multiple times before the 
+            // stream is ready.
+            var tcs = new TaskCompletionSource<object>();
+            if (Interlocked.CompareExchange(ref _beginWriteTask, tcs.Task, null) != null)
+            {
+                // Write when the stream is ready
+                return _beginWriteTask.Then(() => WriteTaskAsync(data)).Catch();
+            }
+
+            _responseMessage.Content = new PushStreamContent((stream, contentHeaders, context) =>
+            {
+                _stream = stream;
+
+                tcs.TrySetResult(null);
+            },
+            new MediaTypeHeaderValue(ContentType));
+
+            // Return the response back to the client
+            _sendResponse();
+
+            // Write when the stream is ready
+            return _beginWriteTask.Then(() => WriteTaskAsync(data)).Catch();
         }
 
         private Task WriteTaskAsync(string data)
@@ -90,7 +99,7 @@ namespace SignalR.Hosting.WebApi
 
         private Task WriteAsync(byte[] buffer)
         {
-            return Task.Factory.FromAsync((cb, state) => _stream.BeginWrite(buffer, 0, buffer.Length, cb, state), 
+            return Task.Factory.FromAsync((cb, state) => _stream.BeginWrite(buffer, 0, buffer.Length, cb, state),
                                            ar => _stream.EndWrite(ar), null);
         }
 
